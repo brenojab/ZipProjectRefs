dd356b6bb26a87ae96d243024cd38c620328f817
Breno Batista <brenojab@gmail.com>
2021-07-27 06:20:54 -0300

    Initial commit.

---
+++ ClassUsingRefProject.cs
@@ -0,0 +1,19 @@
+﻿using ProjUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjRef
+{
+    public class ClassUsingRefProject
+    {
+
+        public void DoSomethingWithRefProject()
+        {
+            ZipUtils.DoSomethingAsGzip();
+
+            //ZipUtils.DoSomethingAsZip();
+        }
+    }
+}
